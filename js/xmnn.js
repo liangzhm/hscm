495cc640f62df36d622c2b124e61e19b845c4481
wenbin zhou <wenbin5243@gmail.com>
2013-04-23 16:23:11 +0800

    添加内页

--- js/xmnn.js
+++ js/xmnn.js
@@ -5,13 +5,29 @@ $(function(){
         var index=$('.tab .h li').index(this);
         $('.tab .b').eq(index).addClass('active').siblings('.b').removeClass('active');
     })
-	//slider
-	$('.slider .b ul').cycle({
+	//数字幻灯m5
+	$('.m5 .b ul').cycle({
         fx:     'scrollLeft',
         speed:  'fast',
-        timeout: 3000,
-        pager:  '.slider .f',
-		prev:    '.prev',
-        next:    '.next'
+        timeout: 5000,
+        pager:  '.m5 .f',
+		prev:    '.m5 .prev',
+        next:    '.m5 .next'
     });
+	//媒体图片m6
+	$('.m6 .bc2 li').click(function(){
+		var href=$(this).find('a').attr('href');
+		$('.m6 .bc1 img').attr('src',href);
+		return false;
+	})
+	$('.m6 .prev').click(function(){
+		$('.m6 .bc2 li:first-child').appendTo('.m6 .bc2 ul');
+		return false;
+	})
+	$('.m6 .next').click(function(){
+		$('.m6 .bc2 li:last-child').prependTo('.m6 .bc2 ul');
+		return false;
+	})
+	//报名参加m7
+	$('.m7 form').html5Validate();
 })
